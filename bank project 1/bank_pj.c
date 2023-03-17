df8171e07e6fb38ea36eaa2d7035f0c216af30fc
AungTunOo9 <120327142+AungTunOo9@users.noreply.github.com>
2023-03-17 22:52:59 +0630

    Add files via upload

---
+++ bank project 1/bank_pj.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "bank_project.h"
+//
+// Created by National Cyber City on 2/6/2023.
+//
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+    currentTime();
+
+//    integer_to_char(1000);
+
+    return 0;
+}
+
